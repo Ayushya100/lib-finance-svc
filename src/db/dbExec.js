a119b6ef9b4512dbdd4df8c4c2933054d29717c6
Ayushya <jaiswalayushya@gmail.com>
2025-02-18 07:18:57 +0530

    Changed db configuration to use knex.js for executing service queries

--- src/db/dbExec.js
+++ src/db/dbExec.js
@@ -5,29 +5,44 @@ import { performance } from 'perf_hooks';
 import { db } from './index.js';
 import { logger } from '../utils/index.js';
 
-const exec = async(query) => {
-    const log = logger('db');
+const log = logger('db');
 
-    log.debug(`Input Query : ${query}`);
-    log.info('Query execution begins');
+const exec = async(query, params = []) => {
+    try {
+        log.debug(`Input Query : ${query}`);
+        log.debug(`Input params : ${params}`);
+        log.info('Query execution begins');
 
-    // Tracker Initiated
-    const startMemo = process.memoryUsage().heapUsed / 1024 / 1024;
-    const startTime = performance.now();
+        // Tracker Initiated
+        const startMemo = process.memoryUsage().heapUsed / 1024 / 1024;
+        const startTime = performance.now();
 
-    const result = await db.execute(query);
-    log.info('Query execution completed');
-    
-    // Tracker Finalized
-    const endMemo = process.memoryUsage().heapUsed / 1024 / 1024;
-    const endTime = performance.now();
+        // Drizzle-orm implementation - deprecated
+        // let result = null;
+        // if (params.length > 0) {
+        //     result = await db.execute(query, params);
+        // } else {
+        //     result = await db.execute(query);
+        // }
+        
+        // Knex.js implementation - in use
+        let result = await db.raw(query, params);
+        log.info('Query execution completed');
+        
+        // Tracker Finalized
+        const endMemo = process.memoryUsage().heapUsed / 1024 / 1024;
+        const endTime = performance.now();
 
-    log.info(`Query execution performance result - Memory Consumption : ${Math.abs((endMemo - startMemo)).toFixed(2)} MB. Time Conumption : ${Math.abs((endTime - startTime)).toFixed(2)} ms`);
+        log.info(`Query execution performance result - Memory Consumption : ${Math.abs((endMemo - startMemo)).toFixed(2)} MB. Time Conumption : ${Math.abs((endTime - startTime)).toFixed(2)} ms`);
 
-    return {
-        rowCount: result.rowCount,
-        rows: result.rows
-    };
+        return {
+            rowCount: result.rowCount,
+            rows: result.rows
+        };
+    } catch (err) {
+        log.error(`Error occurred while executing the query! Error : ${err}`);
+        return null;
+    }
 }
 
 export default exec;
